6ff5c00a8dd7ebbe81d21303b0d7865494ea144b
yamas99 <69020266+yamas99@users.noreply.github.com>
2025-02-19 22:51:28 -0700

    Registration function created
    A page for user account registration was created as register.html. app.py was updated with the appropriate routing. login.html now includes a button direct users to register.html.

--- app.py
+++ app.py
@@ -89,5 +89,16 @@ def logout():
     session.pop('user', None)
     return redirect(url_for('index'))
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
