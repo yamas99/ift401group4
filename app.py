6750ffd37aa71aa8fc56383ebab3b4d686e06690
Chmmrnmhrm <101820703+Chmmrnmhrm@users.noreply.github.com>
2025-02-25 13:02:29 -0700

    Login/out functionality

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required, current_user
+from flask_login import LoginManager, UserMixin, login_user, login_required, current_user, logout_user
 import os
 
 app = Flask(__name__)
@@ -13,7 +13,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 # User model
-class Users(db.Model):
+class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
@@ -92,25 +92,24 @@ def users():
 
 
 @app.route('/admin')
-#@login_required
+@login_required
 def admin():
     return render_template('admin/admin.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        user = Users.query.filter_by(username=username, password=password).first()
-        if user:
-            session['user'] = user.username
-            return redirect(url_for('index'))
-    return render_template('login.html')
+    if request.method == "POST":
+        user = Users.query.filter_by(username=request.form.get("username")).first()
+        if user and user.password == request.form.get("password"):
+            login_user(user)
+            return redirect(url_for("index"))
+    return render_template("login.html")
 
 @app.route('/logout')
+@login_required
 def logout():
-    session.pop('user', None)
-    return redirect(url_for('index'))
+    logout_user()
+    return redirect(url_for("login"))
 
 
 
